45b16581e3526d3585a044b78282ba379b4a1c91
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2024-09-11 08:31:43 +0530

    header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -18,7 +18,7 @@ export const Header = () => {
   };
   return (
     <header className="sticky top-0 backdrop-blur-sm z-20">
-      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+      {/* <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <p className="text-white/60 hidden md:block">
           Streamline your workflow and boost your productivity
         </p>
@@ -26,11 +26,11 @@ export const Header = () => {
           <p>Get started for free</p>
           <ArrowRight className="size-4 inline-flex justify-center items-center" />
         </div>
-      </div>
+      </div> */}
       <div className="py-5">
         <div className="container">
           <div className="flex items-center justify-between">
-            <Image src={Logo} alt="Saas Logo" height={40} width={40} />
+            <Image src={Logo} alt="Saas Logo" height={50} width={50} />
             <MenuIcon className="size-4 md:hidden" />
             <nav className="hidden md:flex gap-6 text-black/60 items-center">
               <button onClick={() => scrollToSection("about")}>About</button>
