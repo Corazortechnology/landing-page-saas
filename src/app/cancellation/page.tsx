4ba66020cda0d03b573ac35842a49010244c47be
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2025-01-26 12:50:18 +0530

    Update page.tsx

--- src/app/cancellation/page.tsx
+++ src/app/cancellation/page.tsx
@@ -3,7 +3,6 @@ import { ReturnAndRefundPolicy } from "@/sections/ReturnAndRefundPolicy";
 export default function CancellationPage() {
     return (
       <>
-          
       <ReturnAndRefundPolicy />
       </>
     )
